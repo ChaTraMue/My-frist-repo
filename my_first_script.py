d30fa5e966aaed5c050982f2aae200d330abc0f6
Chris <christharanisorn@gmail.com>
2026-03-26 15:45:16 +1030

    compound growth function

--- my_first_script.py
+++ my_first_script.py
@@ -1,6 +1,9 @@
-investment = 1000
-rate = 0.05
+def compound_growth(investment, rate, years):
+	for year in range(1, years+1):
+		investment = investment * (1+rate)
+		print("Year", year, ":", round(investment, 2))
 
-for year in range (1, 31):
-	investment = investment * (1+rate)
-	print("Year", year, ":" , round(investment, 2))
+compound_growth(1000, 0.05, 30)
+compound_growth(5000, 0.07, 20)
+compound_growth(500, 0.1, 10)
+ 
